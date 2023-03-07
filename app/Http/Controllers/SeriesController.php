12524836b41f2b1764f0fd22f37f63903e33a79f
Lucas Matheus <lucasmatheus.matheus231@gmail.com>
2023-03-07 12:31:16 -0300

    modificações sendo feitas

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,20 +6,32 @@ use Symfony\Component\HttpFoundation\Request;
 
 
 class SeriesController extends Controller{
-    public function index() {
 
-        $series = serie::query()->orderBy(column: 'nome') -> get();
-        return view('series.index', compact('series'));
+    public function index(Request $request) {
+
+        $series = serie::query()
+            ->orderBy(column: 'nome') 
+            ->get();
+            $mensagem = $request->session()->get(key: 'mensagem');
     
-    }
+            return view('series.index', compact('series'));
 
+    }
     public function create(){
         return view('series.create');
     }
 
-    public function store(Request $request){
-    $serie = Serie::create($request->all());
-
-    return redirect(to: '/series');
-}
+    public function store(Request $request)
+    {
+    
+        Serie::create($request->all());
+        $request->session()
+        ->put(
+            'mensagem',
+            "Série {$serie->id} criada com sucesso {$serie->nome}";
+            );
+    
+    
+        return redirect(to: '/series');
+    }
 }
