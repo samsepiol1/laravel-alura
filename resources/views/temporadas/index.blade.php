0d5dca2c9ac00860c6558a6d5f64a2d58e172a55
Lucas Matheus <steve.jkl5@gmail.com>
2023-03-07 23:07:48 -0300

    caminho para os links

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -7,7 +7,14 @@
 @section('conteudo')
     <ul class="list-group">
         @foreach($temporadas as $temporada)
-            <li class="list-group-item">{{ $temporada->numero }}</li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <a href="#">
+                    Temporada {{ $temporada->numero }}
+                </a>
+                <span class="badge badge-secondary">
+                    {{ $temporada->episodios->count() }}
+                </span>
+            </li>
         @endforeach
     </ul>
 
